a5de98e49697824df34563f014db4a0a37aa1264
ztyjr888 <ztyjr88@163.com>
2015-05-06 10:25:34 +0800

    Create translate_index.php

---
+++ translate/translate_index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link type="text/css" href="../css/translate/translate.css"
+	rel="stylesheet" />
+<script type="text/javascript" src="../js/jquery-1.7.min.js"></script>
+<script type="text/javascript" src="../js/base.js"></script>
+<script type="text/javascript" src="../js/translate.js"></script>
+<title>在线翻译</title>
+</head>
+<body style="background-color: #eee;">
+	<div class="trans-operation clearfix">
+		<a href="###" class="language-btn select-from-language" style="width: 420px;">
+			<span class="from-language">源语言:&nbsp;</span>
+			<span class="language-selected" data-lang="auto" style="width: 165px;">
+				<span>自动检测</span>
+			</span>
+			<i class="caret"></i>
+		</a>
+		
+<!-- 		<img src="../img/other/jiantou.png" class="img"> -->
+			
+		<a href="###" class="language-btn select-to-language"  style="width: 470px;">
+			<span class="to-language">目标语言:&nbsp;</span>
+			<span class="language-selected" data-lang="auto" style="width: 165px;">
+				<span>自动检测</span>
+			</span>
+			<i class="caret"></i>
+		</a>
+	</div>
+	
+	<div class="translate-language from-language-list">
+	    <table class="language-data">
+	        <tbody>
+	            <tr>
+	                <td>
+	                    <a href="###" value="auto" class="lang-selected">
+	                                                                            自动检测
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="ara">
+	                      	  阿拉伯语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="de">
+	                       	 德语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="ru">
+	                       	 俄语
+	                    </a>
+	                </td>
+	            </tr>
+	            <tr>
+	                <td>
+	                    <a href="###" value="fra">
+	                    	    法语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="kor">
+	                      	  韩语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="pt">
+	                                                                       葡萄牙语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="jp">
+	                       	 日语
+	                    </a>
+	                </td>
+	            </tr>
+	            <tr>
+	                <td>
+	                    <a href="###" value="th">
+	                     	   泰语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="wyw">
+	                       	 文言文
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="spa">
+	                       	 西班牙语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="it">
+	                       	 意大利语
+	                    </a>
+	                </td>
+	            </tr>
+	            <tr>
+	                <td>
+	                    <a href="###" value="en">
+	                        	英语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="yue">
+	                       	           粤语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="zh">
+	                        	中文
+	                    </a>
+	                </td>
+	            </tr>
+	        </tbody>
+	    </table>
+	</div>
+	
+	<div class="translate-language from-language-list2" style="top: 250px;">
+	    <table class="language-data">
+	        <tbody>
+	            <tr>
+	                <td>
+	                    <a href="###" value="auto" class="lang-selected">
+	                                                                        自动检测
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="ara">
+	                      	  阿拉伯语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="de">
+	                       	 德语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="ru">
+	                       	 俄语
+	                    </a>
+	                </td>
+	            </tr>
+	            <tr>
+	                <td>
+	                    <a href="###" value="fra">
+	                    	    法语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="kor">
+	                      	  韩语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="pt">
+	                                                                       葡萄牙语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="jp">
+	                       	 日语
+	                    </a>
+	                </td>
+	            </tr>
+	            <tr>
+	                <td>
+	                    <a href="###" value="th">
+	                     	   泰语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="wyw">
+	                       	 文言文
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="spa">
+	                       	 西班牙语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="it">
+	                       	 意大利语
+	                    </a>
+	                </td>
+	            </tr>
+	            <tr>
+	                <td>
+	                    <a href="###" value="en">
+	                        	英语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="yue">
+	                       	           粤语
+	                    </a>
+	                </td>
+	                <td>
+	                    <a href="###" value="zh">
+	                        	中文
+	                    </a>
+	                </td>
+	            </tr>
+	        </tbody>
+	    </table>
+	</div>
+
+	<div class="input-wrap">
+		<div class="textarea-wrap" style="height: 285px;">
+			<div class="textarea-bg">
+				<span class="prompt-text">请输入您要翻译的文字内容,回车即可翻译</span>
+			</div>
+			<div class="textarea-bg-text"></div>
+			<textarea class="textarea" id="translate_input" style="height: 280px;"></textarea>
+		</div>
+		<div class="trans-prompt" style="display: none;">
+			<p class="prompt-wrap"></p>
+		</div>
+	</div>
+	
+	<div class="output-wrap output-blank"></div>
+	
+	<a href="javascript:translate.query();" class="trans-btn" id="translate-button" target="_self">
+			<span class="btn">翻  译</span>
+	</a>
+</body>
+</html>
