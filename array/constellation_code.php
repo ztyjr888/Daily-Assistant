27bc8a9932a4516c4a62f726f4b59c3852345682
ztyjr888 <ztyjr88@163.com>
2015-05-06 10:14:27 +0800

    Create constellation_code.php

---
+++ array/constellation_code.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * 星座
+ */
+$constellation_code = array(
+		"白羊/白羊座"=>"Aries",
+		"金牛/金牛座"=>"Taurus",
+		"双子/双子座"=>"Gemini",
+		"巨蟹/巨蟹座"=>"Cancer",
+		"狮子/狮子座"=>"leo",
+		"处女/处女座"=>"Virgo",
+		"天秤/天秤座"=>"Libra",
+		"天蝎/天蝎座"=>"Scorpio",
+		"射手/射手座"=>"Sagittarius",
+		"摩羯/摩羯座"=>"Capricorn",
+		"水瓶/水瓶座"=>"Aquarius",
+		"双鱼/双鱼座"=>"Pisces"
+);
+
+$constellation_img = array(
+		"白羊/白羊座"=>"img/constellation/img_Aries.gif",
+		"金牛/金牛座"=>"img/constellation/img_Taurus.gif",
+		"双子/双子座"=>"img/constellation/img_Gemini.gif",
+		"巨蟹/巨蟹座"=>"img/constellation/img_Cancer.gif",
+		"狮子/狮子座"=>"img/constellation/img_Leo.gif",
+		"处女/处女座"=>"img/constellation/img_Virgo.gif",
+		"天秤/天秤座"=>"img/constellation/img_Libra.gif",
+		"天蝎/天蝎座"=>"img/constellation/img_Scorpio.gif",
+		"射手/射手座"=>"img/constellation/img_Sagittarius.gif",
+		"摩羯/摩羯座"=>"img/constellation/img_Capricorn.gif",
+		"水瓶/水瓶座"=>"img/constellation/img_Aquarius.gif",
+		"双鱼/双鱼座"=>"img/constellation/img_Pisces.gif"
+);
+
+$constellation_date = array(
+		"Aries"=>"0321,0420",
+		"Taurus"=>"0421,0521",
+		"Gemini"=>"0522,0621",
+		"Cancer"=>"0622,0722",
+		"leo"=>"0723,0822",
+		"Virgo"=>"0823,0922",
+		"Libra"=>"0923,1023",
+		"Scorpio"=>"1024,1122",
+		"Sagittarius"=>"1123,1221",
+		"Capricorn"=>"1222,0120",
+		"Aquarius"=>"0121,0219",
+		"Pisces"=>"0220,0320"
+);
+
+?>
